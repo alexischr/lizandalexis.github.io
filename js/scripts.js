1f39c0993af0c5145d2027bdddf312e403abded9
Alexis Christoforides <alexis@thenull.net>
2025-12-18 11:37:50 -0500

    WIP

--- js/scripts.js
+++ js/scripts.js
@@ -184,20 +184,20 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Ram and Antara's Wedding",
+            title: "Liz and Alexis' Wedding (Boston, MA)",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('May 24, 2026 16:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('May 25, 2026 00:00'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: 'Arlington Memorial Amphitheater, 51 Grove St, Arlington, MA 02474, USA',
 
             // Event Description
             description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
